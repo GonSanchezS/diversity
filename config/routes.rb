c43f9781e3d506abfa51d12ddc49ca02063ac882
Gonzalo <gonsanchezs@gmail.com>
2017-10-27 18:58:07 +0000

    add rankings and data

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
+  get 'about', to:'static_pages#about'
+  get 'ranking', to:'static_pages#ranking'
+  get 'data', to:'static_pages#data'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
